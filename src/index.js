4e53a267a033a9a88d2b4783e9d165a3bc8c84bd
Alexey10031994 <alexseva94@gmail.com>
2022-12-20 17:44:36 +0200

    catch the error fetch API

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 
 const getResource = async (url) => {
   const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}` + `, received ${res.status}`)
+  }
   const body = await res.json();
-
   return body;
 }
 
 getResource('https://swapi.dev/api/people/1/')
-  .then(body => console.log(body));
+  .then(body => console.log(body))
+  .catch(err => console.log(err));
 
 // fetch('https://swapi.dev/api/people/1/')
 //   .then(response => response.json())
