f5d24d610a864d52d01edd69872f50c19d5e5362
Alexey10031994 <alexseva94@gmail.com>
2022-12-20 22:03:27 +0200

    added class componetns + styles PersonDetails

---
+++ src/components/PersonDatails/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+//styles 
+import './personDatails.css';
+
+export default class PersonDatails extends Component {
+    render() {
+        return (
+            <div>PersonDatails</div>
+        );
+    }
+}
