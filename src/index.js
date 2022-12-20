45f4c37c0b1c2530c0133c35fec73dd5aacd1a75
Alexey10031994 <alexseva94@gmail.com>
2022-12-20 17:31:34 +0200

    fetch API

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './themeDarcly.css';
 import App from './App';
 
+const getResource = async (url) => {
+  const res = await fetch(url);
+  const body = await res.json();
+
+  return body;
+}
+
+getResource('https://swapi.dev/api/people/1/')
+  .then(body => console.log(body));
+
+// fetch('https://swapi.dev/api/people/1/')
+//   .then(response => response.json())
+//   .then(body => console.log(body));
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
