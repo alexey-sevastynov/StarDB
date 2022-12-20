cc8ed4b702a5e4aa1a3d2db265d069a53d699733
Alexey10031994 <alexseva94@gmail.com>
2022-12-20 21:52:36 +0200

    added class component RandomPlanet

---
+++ src/components/ReandomPlanet/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RandomPlanet extends Component {
+    render() {
+        return (
+            <div className='random-planet'>
+                <img src="" alt="#" />
+            </div>
+        );
+    }
+}
