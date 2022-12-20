8d9421253006483d5b1a4c2e5a6c57feca754bab
Alexey10031994 <alexseva94@gmail.com>
2022-12-20 22:08:00 +0200

    updated App.jsx

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import React from "react";
 //styles
 import './app.css';
 
+//components
 import Header from "./components/Header";
+import ItemList from "./components/ItemList";
+import PersonDatails from "./components/PersonDatails";
+import RandomPlanet from "./components/ReandomPlanet";
 
 function App() {
-  console.log('hello');
+
 
   return (
     <div className="App">
       <Header />
+      <RandomPlanet />
+
+      <div className="row mb2">
+        <div className="col-md-6">
+          <ItemList />
+        </div>
+        <div className="col-md-6">
+          <PersonDatails />
+        </div>
+      </div>
     </div>
   );
 }
