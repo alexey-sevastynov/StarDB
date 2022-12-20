5decb0e0f10d5ee4dcf3a38fa8db013ecc639dcd
Alexey10031994 <alexseva94@gmail.com>
2022-12-20 22:16:17 +0200

    updated ItemList.jsx

--- src/components/ItemList/index.js
+++ src/components/ItemList/index.js
@@ -1,9 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import './itemList.css';
 
 export default class ItemList extends Component {
   render() {
     return (
-      <div>ItemList</div>
+      <ul className='item-list list-group'>
+        <li className='list-group-item'>
+          Lule Skywalker
+        </li>
+        <li className='list-group-item'>
+          Darth Vader
+        </li>
+        <li className='list-group-item'>
+          R2-D2
+        </li>
+      </ul>
     )
   }
 }
