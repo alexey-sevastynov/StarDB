a2e85e7b82cfc5224f03dea8a7742019e8886e11
Alexey10031994 <alexseva94@gmail.com>
2022-12-22 18:45:12 +0200

    added logic loading in component RandomPlanet

--- src/components/ReandomPlanet/index.js
+++ src/components/ReandomPlanet/index.js
@@ -13,7 +13,7 @@ export default class RandomPlanet extends Component {
 
     state = {
         planet: {},
-
+        loading: true,
     }
 
     constructor() {
@@ -22,7 +22,7 @@ export default class RandomPlanet extends Component {
     }
 
     onPlanetLoaded = (planet) => {
-        this.setState({ planet });
+        this.setState({ planet, loading: false });
     };
 
     updatePlanet() {
@@ -37,34 +37,37 @@ export default class RandomPlanet extends Component {
 
     render() {
 
-        const { planet: { id, name, population, rotationPeriod, diameter } } = this.state;
+        const {
+            planet: { id, name, population, rotationPeriod, diameter },
+            loading } = this.state;
 
         return (
-
-
             <div className='random-planet jumbotron rounded'>
-                <Spinner />
-                {/* <img
-                    className='planet-image'
-                    src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-                    alt="img" />
-                <div className='ml-20'>
-                    <h4>{name}</h4>
-                    <ul className='list-group list-group-flush'>
-                        <li className='list-group-item'>
-                            <span className='term'>Population</span>
-                            <span className='ml-20'>{population}</span>
-                        </li>
-                        <li className='list-group-item'>
-                            <span className='term'>Rotation Period</span>
-                            <span className='ml-20'>{rotationPeriod}</span>
-                        </li>
-                        <li className='list-group-item'>
-                            <span className='term'>Diameter</span>
-                            <span className='ml-20'>{diameter}</span>
-                        </li>
-                    </ul>
-                </div> */}
+                {loading ? <Spinner /> :
+                    <>
+                        <img
+                            className='planet-image'
+                            src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+                            alt="img" />
+                        <div className='ml-20'>
+                            <h4>{name}</h4>
+                            <ul className='list-group list-group-flush'>
+                                <li className='list-group-item'>
+                                    <span className='term'>Population</span>
+                                    <span className='ml-20'>{population}</span>
+                                </li>
+                                <li className='list-group-item'>
+                                    <span className='term'>Rotation Period</span>
+                                    <span className='ml-20'>{rotationPeriod}</span>
+                                </li>
+                                <li className='list-group-item'>
+                                    <span className='term'>Diameter</span>
+                                    <span className='ml-20'>{diameter}</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </>
+                }
             </div>
         );
     };
